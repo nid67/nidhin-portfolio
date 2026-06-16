1ac2b25225a7a1fe179c5bcc79890c2061c5b590
blade-67 <nidhin.240094@aiml.ritchennai.edu.in>
2026-06-16 22:15:08 +0530

    update year to 2026 and link resume securely

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -74,7 +74,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="/resume/Nidhin_s_Resume.pdf" target="_blank">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
